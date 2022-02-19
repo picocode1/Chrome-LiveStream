bcd11402462eef754fc995b28c5d3e694744702d
picocode <38665815+picocode1@users.noreply.github.com>
2022-02-20 00:24:47 +0100

    Added HTTP server

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const WebSocket = require('ws')
-const port = 6969
-const Server = new WebSocket.Server({port: port})
+const http = require("http");
+const express = require('express');
 
-console.log(`Starting server on port ${port}`)
+const WS_PORT = 6969
+const HTTP_PORT = 80;
+const HTTP_HOST = '127.0.0.1';
+
+const Server = new WebSocket.Server({port: WS_PORT})
+
+console.log(`WebSocket: Starting server on port ${WS_PORT}`)
+
+console.log(`Start LiveStream from http://${HTTP_HOST}:${HTTP_PORT}/screenshare`)
+console.log(`Watch LiveStream from http://${HTTP_HOST}:${HTTP_PORT}/livestream\n`)
+
+
+//Use express
+var app = express();
+app.use(express.static('public'))
+
+//Send livestream file
+app.get('/livestream', function(req, res) {
+    res.sendFile(__dirname + '/livestream.html')
+})
+
+//Send screenshare file
+app.get('/screenshare', function(req, res) {
+    res.sendFile(__dirname + '/screenshare.html')
+})
+
+//Start webserver
+var server = app.listen(HTTP_PORT, function(){
+    console.log(`HTTP Server is running on http://${HTTP_HOST}:${HTTP_PORT}`);
+});
+
+
+
 
 //Function to only send data to everyone except the sender so we dont send the livestream back to the 
 function ScreenCapture(data, sender) {
